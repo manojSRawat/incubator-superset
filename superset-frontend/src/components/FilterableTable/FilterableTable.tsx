989a9a27401e36fe678e668c35bcf4657d4e8e40
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-02-05 14:37:02 +0530

    fix

--- superset-frontend/src/components/FilterableTable/FilterableTable.tsx
+++ superset-frontend/src/components/FilterableTable/FilterableTable.tsx
@@ -391,6 +391,7 @@ export default class FilterableTable extends PureComponent<
             ...style,
             top:
               typeof style.top === 'number'
+              // @ts-ignore
                 ? style.top - GRID_POSITION_ADJUSTMENT
                 : style.top,
           }}
@@ -423,6 +424,7 @@ export default class FilterableTable extends PureComponent<
           ...style,
           top:
             typeof style.top === 'number'
+            // @ts-ignore
               ? style.top - GRID_POSITION_ADJUSTMENT
               : style.top,
         }}
